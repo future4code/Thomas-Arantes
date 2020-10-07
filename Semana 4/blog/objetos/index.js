a7e6323b7c9c085211bbb2654931e7878fcaddd4
Thomas Arantes <thomas.arantes97@gmail.com>
2020-10-07 15:41:25 -0300

    o desafio 1 está feito

--- Semana 4/blog/objetos/index.js
+++ Semana 4/blog/objetos/index.js
@@ -1,5 +1,6 @@
 const inputTitulo = document.getElementById("titulo-post")
 const inputAutor = document.getElementById("autor-post")
+const inputImagem = document.getElementById("imagem-post")
 const inputConteudo = document.getElementById("conteudo-post")
 const containerBlog = document.getElementById("container-de-posts")
 let arrayPost = []
@@ -7,12 +8,14 @@ let arrayPost = []
 const postBlog = {
     titulo: inputTitulo.value,
     autor: inputAutor.value,
+    imagem: inputImagem.value,
     conteudo: inputConteudo.value
 }
 
 function adicionarPost(){
     postBlog.titulo = inputTitulo.value
     postBlog.autor = inputAutor.value
+    postBlog.imagem = inputImagem.value
     postBlog.conteudo = inputConteudo.value
     
     arrayPost.push(postBlog.titulo, postBlog.autor, postBlog.conteudo)
@@ -23,5 +26,11 @@ function adicionarPost(){
     inputConteudo.value = ""
 
     containerBlog.innerHTML += `<h2> ${postBlog.titulo} </h3> <h5> ${postBlog.autor} <p> ${postBlog.conteudo} </p> `
+
+    if (postBlog.imagem.includes('.png') || postBlog.imagem.includes('.jpg')){
+        containerBlog.innerHTML += `<img src=${postBlog.imagem}>`
+    } else {
+        alert('Link incorreto! Insira imagem válida')
+    }
 }
 
