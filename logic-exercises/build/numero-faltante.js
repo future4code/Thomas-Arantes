9e9b2a48756d192748870715836d6e897df3fe95
Thomas Arantes <thomas.arantes97@gmail.com>
2021-03-23 16:11:25 -0300

    exercicio indexOf

--- logic-exercises/build/numero-faltante.js
+++ logic-exercises/build/numero-faltante.js
@@ -8,7 +8,6 @@ const missingNumber = (array) => {
         sum += number;
     }
     return expectedSum - sum;
-    console.log(expectedSum - sum);
 };
 exports.missingNumber = missingNumber;
 exports.missingNumber([1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12]);
