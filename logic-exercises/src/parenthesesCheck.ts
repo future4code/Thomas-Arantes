a418f067676bf4b080fef95465e24873d48a8c08
Thomas Arantes <thomas.arantes97@gmail.com>
2021-03-30 15:00:28 -0300

    exercicio anagrama

--- logic-exercises/src/parenthesesCheck.ts
+++ logic-exercises/src/parenthesesCheck.ts
@@ -1,31 +1,31 @@
-function parethesesCheck(string) {
-    const stack = [];
+// function parethesesCheck(string) {
+//     const stack = [];
 
-    for (let character of string) {
-        if (character === "(" || character === "[" || character === "{") {
-            stack.push(character)
-        } 
-        else {
-            const lastOpeningCharacter = stack.pop();
-            if(!lastOpeningCharacter) {
-                return false
-            }
-            else if (
-                lastOpeningCharacter === "(" && character !== "(" ||
-                lastOpeningCharacter === "[" && character !== "[" ||
-                lastOpeningCharacter === "{" && character !== "{"
-            ) {
-                return false
-            }
-        }
-    }
+//     for (let character of string) {
+//         if (character === "(" || character === "[" || character === "{") {
+//             stack.push(character)
+//         } 
+//         else {
+//             const lastOpeningCharacter = stack.pop();
+//             if(!lastOpeningCharacter) {
+//                 return false
+//             }
+//             else if (
+//                 lastOpeningCharacter === "(" && character !== "(" ||
+//                 lastOpeningCharacter === "[" && character !== "[" ||
+//                 lastOpeningCharacter === "{" && character !== "{"
+//             ) {
+//                 return false
+//             }
+//         }
+//     }
 
-    if (stack.length > 0) {
-        return false
-    }
+//     if (stack.length > 0) {
+//         return false
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
-parethesesCheck("()")
+// parethesesCheck("()")
 
